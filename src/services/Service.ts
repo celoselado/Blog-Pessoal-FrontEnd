2de63b6c732558d6fde0d2ca57a967a73b306fc6
unknown <malexandresantos@hotmail.com>
2023-05-24 12:48:06 -0300

    tasks

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,12 +4,19 @@ export const api = axios.create({
     baseURL: 'https://blogpessoal-5z1b.onrender.com'
 })
 
-export const login = async(url: any, dados: any, setDado: any ) => {
+
+export const cadastroUsuario = async(url: any, dados: any, setDado: any ) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data)
 } 
 
-export const cadastroUsuario = async(url: any, dados: any, setDado: any ) => {
+export const login = async(url: any, dados: any, setDado: any ) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data.token)
+}
+
+export const busca = async(url: any, setDado: any, header: any) => {
+    const resposta = await api.get(url, header)
+    setDado(resposta.data)
 } 
+
